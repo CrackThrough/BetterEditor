8f31a1d03017177343f7b29efb283f30770504a6
CrackThrough <59867573+CrackThrough@users.noreply.github.com>
2021-02-20 21:25:43 +0900

    Add project files

---
+++ README.md
@@ -0,0 +1,2 @@
+# BetterEditor
+This is a mod for a game called "A Dance of Fire and Ice" to make editor a bit better for everyone to use.
--- sendtodir.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-fs.copyFileSync(
-    "./info.json",
-    "C:/Program Files (x86)/Steam/steamapps/common/A Dance of Fire and Ice/Mods/BetterEditor/info.json"
-);
-fs.copyFileSync(
-    "./bin/Debug/BetterEditor.dll",
-    "C:/Program Files (x86)/Steam/steamapps/common/A Dance of Fire and Ice/Mods/BetterEditor/BetterEditor.dll"
-);
