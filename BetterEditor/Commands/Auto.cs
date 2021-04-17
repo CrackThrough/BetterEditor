16933af078c6d6ca66072b0eb920a55f968af5dd
DESKTOP-QPBRLN3\user <jaey20179@gmail.com>
2021-04-18 02:30:16 +0900

    Added auto, save command

---
+++ BetterEditor/Commands/Auto.cs
@@ -0,0 +1,61 @@
+﻿using BetterEditor.Core;
+using BetterEditor.Core.Attributes;
+using UnityEngine;
+
+namespace BetterEditor.Commands
+{
+    [CommandInfo(Id = "auto")] 
+    class Auto : BECommand
+    {
+        Auto() : base() { }
+
+        public override void Execute(scnEditor instance, string[] args)
+        {
+            if (args.Length == 0) RDC.auto = !RDC.auto;
+
+            switch (args[0].ToLower())
+            {
+                case "on":
+                    RDC.auto = true;
+                    break;
+                case "off":
+                    RDC.auto = false;
+                    break;
+                case "toggle":
+                    RDC.auto = !RDC.auto;
+                    break;
+                case "get":
+                    break;
+                default:
+                    return;
+            }
+
+            scnEditorPrivates.SetField("autoFailed", false);
+
+            AudioSource ottoSrc = scnEditorPrivates.GetField<AudioSource>("ottoSrc");
+            bool highBPM = scnEditorPrivates.GetField<bool>("highBPM");
+            AudioClip[] ottoClips = scnEditorPrivates.GetField<AudioClip[]>("ottoClips");
+
+            if (RDC.auto)
+            {
+                if (highBPM)
+                {
+                    ottoSrc.clip = ottoClips[1];
+                }
+                else
+                {
+                    ottoSrc.clip = ottoClips[0];
+                }
+            }
+            else
+            {
+                ottoSrc.clip = ottoClips[3];
+            }
+
+            if (args[0].ToLower() != "get")
+            {
+                ottoSrc.Play();
+            }
+        }
+    }
+}
---
+++ BetterEditor/Commands/Save.cs
@@ -0,0 +1,31 @@
+﻿using ADOFAI;
+using BetterEditor.Core;
+using BetterEditor.Core.Attributes;
+using UnityEngine;
+
+namespace BetterEditor.Commands
+{
+    [CommandInfo(Id = "save")]
+    class Save : BECommand
+    {
+        Save() : base() { }
+
+        public override void Execute(scnEditor instance, string[] args)
+        {
+            if (args.Length == 0) return;
+
+            switch (args[0].ToLower())
+            {
+                case "as":
+                    if (!string.IsNullOrEmpty(args[1]))
+                    {
+                        RDFile.WriteAllText(args[1], scnEditorPrivates.GetField<LevelData>("levelData").Encode());
+                    }
+                    break;
+                default:
+                    scnEditorPrivates.InvokeMethod("SaveLevel");
+                    return;
+            }
+        }
+    }
+}
